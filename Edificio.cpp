e6a7e0b8232cde619944d504d92409e066828ccc
dcartwright10 <davidcb94@gmail.com>
2019-05-07 01:17:19 -0500

    segundo commit

---
+++ Edificio.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+double forzamiento(t,omega);
+	
+
+int main(){
+	double tot_t = 10;
+	double dt = 0.1;
+	double m = 1000;
+	double gamma = 0;
+	double k = 2000;
+	double omega = sqrt(k/m);
+	double *vt;
+	vt = new double [n];
+	int n = int (tot_t/dt);
+	double *u1;
+	u1 = new double [n]; 	
+	double *u2;
+	u2 = new double [n];
+	double *u3;
+	u3 = new double [n];
+	double *v1;
+	v1 = new double [n];
+	double *v2;
+	v2 = new double [n];
+	double *v3;
+	v3 = new double [n];
+	u1[0]=0,u2[0]=0,u3[0]=0;
+	v1[0]=0,v2[0]=0,v3[0]=0;
+	vt[0]=0;
+	double k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14,k15,k16,k17,k18,k19,k20,k21,k22,k23,k24;
+	
+	
+	for(int i=1; i<=n; i++){
+		vt[i]= vt[i-1]+dt;		
+		k1 = dt*(1/m)*(-gamma*v1[i-1]-2*k*u1[i-1]+k*u2[i-1]+ forzamiento(t[i-1],omega);
+		k2 = dt*(1/m)*(-gamma*u1[i-1]-2*k*u1[i-1]+k*u2[i-1]+ forzamiento(t[i-1],omega);
+		k7= dt*(1/m)*(-gamma*(v1[i-1]+k1/2)-2*k*(u1[i-1]+k4/2)+k*(u2[i-1]+k5/2)+ forzamiento((t[i-1]+dt/2),omega);
+		
+
+		u1[i] = u1[i-1]+(1/6)*(k4+2*k10+2*k16+k22)
+		u2[i] = u2[i-1]
+		}
+	ofstream file;
+	file.open("datos.dat");
+	for(int j=0; j<=n; j++){
+		file << vt[i] <<" "<< u1[i] <<" "<< u2[i] <<" "<< u3[i] <<" "<< v1[i] <<" "<< v2[i] <<" "<< v3[i] <<endl;
+		}
+	file.close();
+	return 0;
+	}
+	double forzamiento(t,omega){
+		return sin(omega*t)
+	}
+	
--- Fourier.py
+++ Fourier.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pylab as plt
-
+from scipy.fftpack import *
 ##Almacenamiento de los datos##
 data1 = np.genfromtxt("signalSuma.dat")
 data2 = np.genfromtxt("signal.dat")
+
+N = 2048
+dt = abs(data1[1,0]-data1[2,0])
+
 
 ##subplots con el grafico para cada senal##
 plt.figure()
@@ -21,15 +25,80 @@ plt.plot(data2[:,0],data2[:,1])
 
 plt.savefig("signal_subplots.pdf")
 
+
+
 ##implementacion de la transformada discreta de fourier##
+def Fourier(N,data):
+	amplitudes = []
+	for n in range(N):
+		suma = 0
+		for k in range(N):			
+			suma += data[k]*np.exp(-2j*np.pi*k*(n/N))
+		print(suma)
+		amplitudes.append(suma)
+	return np.asarray(amplitudes)
+
+#res = abs(Fourier(N,data1[:,1]))
+#res2 = abs(Fourier(N,data2[:,1]))
+freq = fftfreq(N,d=dt)
+
+####Prueba de grafico####
+sci_res = fft(data1[:,1])
+plt.figure()
+plt.plot(freq,abs(sci_res))
+plt.xlim(0,3000)
+plt.show()
 
 ##graficos de la transformada de Fourier de ambas senales##
+
+#plt.figure()
+#plt.plot(res)
+#plt.plot(fftfreq(N,d=dt),res2)
+#plt.show()
+
 
 ##espectrograma de las senales##
 plt.figure()
 plt.subplot(2,1,1)
 plt.specgram(data1[:,1])
+plt.ylabel("Frecuencia(Hz)")
+plt.title("Suma senales")
+
 
 plt.subplot(2,1,2)
 plt.specgram(data2[:,1])
+plt.xlabel("tiempo(s)")
+plt.title("senales individuales")
+plt.show()
+
+##Almacenamiento de datos y grafica de la senal "temblor" en funcion de tiempo##
+
+temblor = np.genfromtxt("temblor(1).txt", skip_header=4)
+Num = len(temblor)
+dt1= (0.01)
+
+plt.figure()
+plt.plot(temblor[:],c="red")
+plt.title("Grafica de senal sismica(freq. muestreo = 100Hz)")
+plt.xlabel("tiempo(s)")
+plt.ylabel("Amplitud(d.u.)")
+plt.show()
+
+##Transformada de Fourier de la senal##
+
+freq_t = fftfreq(Num,d=dt1)
+Trans_temblor = fft(temblor)
+plt.figure()
+plt.plot(freq_t,abs(Trans_temblor))
+plt.title("Transformada de Fourier de senal de temblor")
+plt.xlabel("Frecuencia")
+plt.ylabel("Densidad de Frecuencia")
+plt.show()
+
+##espectrograma de senal de temblor##
+plt.figure()
+plt.specgram(temblor)
+plt.title("Espectrograma de la senal de temblor")
+plt.xlabel("tiempo(s)")
+plt.ylabel("Frecuencia(Hz)")
 plt.show()
